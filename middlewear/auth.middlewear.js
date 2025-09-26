8663a6ed70045582b261a63e378a6456c900d2fd
yaswanthnaidu-yalla <yaswanthnaiduyalla@gmail.com>
2025-09-26 22:48:46 +0530

    fixed jwt token for adminauthsecurity purposes

--- middlewear/auth.middlewear.js
+++ middlewear/auth.middlewear.js
@@ -8,7 +8,7 @@ const authorize = async (req, res, next) => {
             token = req.headers.authorization.split(' ')[1];}   
         if (!token) return res.status(401).json({message: 'Not authorized, no token'});  
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user= await User.findById(decoded.userId).select('-password');
+        const user= await User.findById(decoded.id).select('-password');
         if(!user) return res.status(401).json({message: 'Not authorized, user not found'});
         req.user = user;
         next();
