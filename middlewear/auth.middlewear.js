536fefb5a3dce7d0f5fbd63e5a210513af5ba9c8
yaswanthnaidu-yalla <yaswanthnaiduyalla@gmail.com>
2025-09-30 13:08:55 +0530

    edited the middlewears and controllers

--- middlewear/auth.middlewear.js
+++ middlewear/auth.middlewear.js
@@ -13,7 +13,7 @@ const authorize = async (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id).select('-password');
+        const user = await User.findById(decoded.userId).select('-password');
         
         if (!user) {
             return res.status(401).json({ message: 'Not authorized, user not found' });
