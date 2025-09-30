24d3321cef7e586e9c0b0fa0c5936787b5602baf
yaswanthnaidu-yalla <yaswanthnaiduyalla@gmail.com>
2025-09-30 09:34:54 +0530

    added user controller logic

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,32 +1,98 @@
 import e from "express";
 import User from "../models/user.model.js";
+
 const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await User.find({}, '-password -__v'); 
-        res.status(200).json({
-            success: true,
-            message: 'Users fetched successfully',
-            data: users
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const users = await User.find({}, '-password -__v');
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully',
+      data: users
+    });
+  } catch (error) {
+    next(error);
+  }
 }
+
 const getUser = async (req, res, next) => {
-    try {
-        const user = await User.findById((req.params.id), '-password -__v');
-        res.status(200).json({
-            success: true,
-            message: 'User fetched successfully',
-            data: user
-        });
-        if (!User) {
-            const error = new Error('User not found');
-            error.statusCode = 404;
-            throw error;
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const user = await User.findById(req.params.id, '-password -__v');
+    
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
     }
+    
+    res.status(200).json({
+      success: true,
+      message: 'User fetched successfully',
+      data: user
+    });
+  } catch (error) {
+    next(error);
+  }
 }
-export { getAllUsers, getUser };
+
+const createUser = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    const userResponse = newUser.toObject();
+    delete userResponse.password;
+    delete userResponse.__v;
+    
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      data: userResponse
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    ).select('-password -__v');
+    
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    
+    res.status(200).json({
+      success: true,
+      message: 'User updated successfully',
+      data: user
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    
+    res.status(200).json({
+      success: true,
+      message: 'User deleted successfully',
+      data: null
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { getAllUsers, getUser, createUser, updateUser, deleteUser };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,14 +3,14 @@ import { JWT_SECRET } from "../config/env.js";
 import authorize from "../middlewear/auth.middlewear.js";
 import errorMiddlewear from "../middlewear/error.middlewear.js";
 
-import { getAllUsers } from "../controller/user.controller.js";   
+import { createUser, deleteUser, getAllUsers, updateUser } from "../controller/user.controller.js";   
 import { getUser } from "../controller/user.controller.js";
 import { get } from "mongoose";
 
 const userrouter = Router();
-userrouter.get('/', getAllUsers);
+userrouter.get('/',authorize, getAllUsers);
 userrouter.get('/:id', authorize,errorMiddlewear,getUser);
-userrouter.post('/', (req, res) => res.send({title:'CREATE User'}));
-userrouter.put('/:id', (req, res) => res.send({title:'UPDATE Users'}));
-userrouter.delete('/:id', (req, res) => res.send({title:'DELETE Users'}));
+userrouter.post('/', authorize, createUser);
+userrouter.put('/:id', authorize, updateUser);
+userrouter.delete('/:id', authorize, deleteUser);
 export default userrouter;
